b4facb06cf2f82ca4351bcbb1c248ba2b19babc6
Paweł Sroka <srokap@gmail.com>
2012-12-30 04:12:16 +0100

    Plugins structure rebuild. Extracted elements to srokap_framework

--- wwwroot/mod/srokap_plugin_crawler/languages/en.php
+++ wwwroot/mod/srokap_plugin_crawler/languages/en.php
@@ -1,10 +1,5 @@
 <?php
 $english = array(
-	//srokap_framework
-	'menu:page:header:srokap' => 'Srokap plugins',
-	'srokap_framework:command:wget' => 'Wget',
-	'srokap_framework:command:svn' => 'SVN',
-
 	//srokap_plugin_crawler
 	'admin:srokap_plugins_crawler' => 'Plugin crawler',
 	'admin:srokap_plugins_crawler:commands' => 'Commands status',
--- wwwroot/mod/srokap_plugin_crawler/start.php
+++ wwwroot/mod/srokap_plugin_crawler/start.php
@@ -1,3 +1,2 @@
 <?php
-elgg_register_event_handler('init', 'system', array('srokap_framework', 'init'));
 elgg_register_event_handler('init', 'system', array('srokap_plugin_crawler', 'init'));
